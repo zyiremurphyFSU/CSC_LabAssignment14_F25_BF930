936a9e0a248f699a5fc91c024923fc7d2868f0df
Zyire <zmurphy5@broncos.uncfsu.edu>
2025-11-21 11:06:33 -0500

    lab2.py finish

--- lab2.py
+++ lab2.py
@@ -15,31 +15,31 @@ and write the final result into a new file.
 
 
 def count_paint_bottles(input_file, output_file):
-    # TODO: create an empty dictionary called `totals` to count all bottles
+    totals = {}
 
-    # TODO: open the file (argument `input_file`) with mode "r"
+    file_obj = open(input_file, "r")
 
-    # TODO: use for loop to iterate the file object and get every line
-        # TODO: use split() function to separate each line into a list, the first item of the list is the color, the second item is the number
+    for line in file_obj:
+        fields = line.strip().split(",")
         color = fields[0]
-        num = #TODO: finish this line, so `num` is the second item in the list (convert to integer)
+        num = int(fields[1])
 
         if color not in totals:
-            # TODO: if the dictionary doesn't record the color yet, add one new entry (the key is the color, the value is the num)
+            totals[color] = num
         else:
-            # TODO: if the dictionary has the color already, we add the num into the existing value
+           totals[color] += num
 
-    # TODO: close the file object
+    file_obj.close()
 
 
-    # TODO: open the output file (argument `output_file`) with mode "w", name the file object as `file_obj2`
+    file_obj2 = open(output_file, "w")
 
     for color in totals.keys():
-        # TODO: get the num of each color, convert it to a string `num_str`
+        num_str = str(totals[color])
         string_to_write = color + ":" + num_str # comebine the string
 
-        # TODO: write the string `string_to_write` with file object
-        # TODO: write a new line with the file object
+        file_obj2.write(string_to_write)
+        file_obj2.write("\n")
 
     file_obj2.close()
 
